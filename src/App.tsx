39f9469798739c8cdd58ef5ea51eb151ec1cad30
Ayeah Godlove A <ayeahgodlove5@gmail.com>
2023-03-23 09:27:35 +0100

    add authentication using auth0

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { ConfigProvider } from "antd";
 import { ThemeConfig } from "antd/es/config-provider/context";
 import { Provider } from "react-redux";
 import store from "Redux/store";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { Auth0Config } from "Config/Constant";
+import { useNavigate } from "react-router-dom";
 
 const theme: ThemeConfig = {
   token: {
@@ -12,12 +15,28 @@ const theme: ThemeConfig = {
   },
 };
 
+const onRedirectCallback = (appState: any) => {
+  const history = useNavigate();
+  history(
+    appState && appState.returnTo ? appState.returnTo : window.location.pathname
+  );
+};
+
 function App() {
   return (
     <Provider store={store}>
+      <Auth0Provider
+        domain={Auth0Config.DOMAIN}
+        clientId={Auth0Config.CLIENT_ID}
+        redirectUri={Auth0Config.REDIRECT_URI}
+        audience={Auth0Config.AUDIENCE}
+        scope={Auth0Config.SCOPE}
+        onRedirectCallback={onRedirectCallback}
+      >
         <ConfigProvider theme={theme}>
           <AppRouteProvider />
         </ConfigProvider>
+      </Auth0Provider>
     </Provider>
   );
 }
